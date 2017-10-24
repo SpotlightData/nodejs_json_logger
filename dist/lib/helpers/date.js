8423f4eb8ad2d0de69151ceb91a097da28c53f1d
Will Evans <will@will3942.com>
2017-10-24 11:30:01 +0100

    Update logger

--- dist/lib/helpers/date.js
+++ dist/lib/helpers/date.js
@@ -1,11 +1,1 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-	value: true
-});
-var generateISOString = function generateISOString() {
-	var d = new Date();
-	return d.toISOString();
-};
-
-exports.generateISOString = generateISOString;
+"use strict";Object.defineProperty(exports,"__esModule",{value:true});var generateISOString=function generateISOString(){var d=new Date();return d.toISOString();};exports.generateISOString=generateISOString;
